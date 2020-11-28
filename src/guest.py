47bfac0aefe2e656953e8067b7f52176c5f9b92c
Evelyn Szarek <evelynszarek@gmail.com>
2020-11-28 20:37:38 +0000

    classes set up

---
+++ src/guest.py
@@ -0,0 +1,16 @@
+class Guest:
+
+    def __init__(self, name, budget, fav_song):
+        self.name = name
+        self.budget = budget
+        self.fav_song = fav_song
+
+    def pay():
+        pass
+
+    def pick_song():
+        pass
+
+    def cheer_loudly():
+        pass
+    
---
+++ src/room.py
@@ -0,0 +1,19 @@
+class Room:
+
+    def __init__(self, room_name, capacity, till, song_played):
+        self.room_name = room_name
+        self.capacity = capacity
+        self.till = till
+        self.song_played = song_played
+
+    def check_in_guest():
+        pass
+
+    def play_song():
+        pass
+
+    def refuse_entry():
+        pass
+
+    def check_out_guest():
+        pass
